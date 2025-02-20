6b0029ac6a344eb70163d59dcb4e776ba9bba896
HP <HP@DESKTOP-6T66JUP>
2025-02-20 22:29:52 +0530

    used setters

--- myproject/src/test/java/com/myproject/myproject/AppTest.java
+++ myproject/src/test/java/com/myproject/myproject/AppTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -14,40 +15,37 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class AppTest {
-	
-String proPath=System.getProperty("user.dir");
-Properties properties = new Properties();
-static ExtentReports extent;
-static ExtentTest test;
 
-  @Test
-  public void demoTest() throws IOException, InterruptedException {
-	  ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("ExtentReportExample.html");
-	  extent = new ExtentReports();
-      extent.attachReporter(htmlReporter);
+	String proPath = System.getProperty("user.dir");
+	Properties properties = new Properties();
+	static ExtentReports extent;
+	static ExtentTest test;
 
-      // Create a new test and start logging
-      test = extent.createTest("SeleniumTest", "This is a test to demonstrate Extent Reports with Selenium");
+	@Test
+	public void demoTest() throws IOException, InterruptedException {
+		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("ExtentReportExample.html");
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
 
-      
-	 WebDriver driver=new ChromeDriver();
-	 FileInputStream input = new FileInputStream("../myproject/config.properties");
-     properties.load(input);
-     String BaseUrl = properties.getProperty("web.BaseURL");
-     
-    //navigate to URL
-	 driver.get(BaseUrl);
-	 test.info("Opened the website: https://www.google.com");
+		// Create a new test and start logging
+		test = extent.createTest("SeleniumTest", "This is a test to demonstrate Extent Reports with Selenium");
 
-	 Thread.sleep(1000);
-	 //download pdf file
-	 
-	 
-	//search for amazon india
-	 driver.findElement(By.xpath("//textarea[@class='gLFyf']")).sendKeys("Amazon");
-	 Thread.sleep(1000);
-     System.out.println("test");
-     driver.close();
-     extent.flush();
-  }
+		WebDriver driver = new ChromeDriver();
+		FileInputStream input = new FileInputStream("../myproject/config.properties");
+		properties.load(input);
+		String BaseUrl = properties.getProperty("web.BaseURL");
+
+		// navigate to URL
+		driver.get(BaseUrl);
+		test.info("Opened the website: https://www.google.com");
+
+		Thread.sleep(1000);
+		// search for amazon india
+		AppDetails ap = PageFactory.initElements(driver, AppDetails.class);
+		ap.setSearchNameField("Amazon");
+
+		Thread.sleep(1000);
+		driver.close();
+		extent.flush();
+	}
 }
