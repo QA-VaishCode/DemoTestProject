f89f9dc20f01104216677894c17e9937a70e6c21
HP <HP@DESKTOP-6T66JUP>
2025-02-20 22:08:55 +0530

    added extent report code

--- myproject/src/test/java/com/myproject/myproject/AppTest.java
+++ myproject/src/test/java/com/myproject/myproject/AppTest.java
@@ -1,18 +1,53 @@
 package com.myproject.myproject;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class AppTest {
 	
 String proPath=System.getProperty("user.dir");
+Properties properties = new Properties();
+static ExtentReports extent;
+static ExtentTest test;
+
   @Test
-  public void shouldAnswerWithTrue() {
+  public void demoTest() throws IOException, InterruptedException {
+	  ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("ExtentReportExample.html");
+	  extent = new ExtentReports();
+      extent.attachReporter(htmlReporter);
+
+      // Create a new test and start logging
+      test = extent.createTest("SeleniumTest", "This is a test to demonstrate Extent Reports with Selenium");
+
+      
 	 WebDriver driver=new ChromeDriver();
-	 driver.get("https://www.google.com/");
-    System.out.println("test");
-    driver.close();
+	 FileInputStream input = new FileInputStream("../myproject/config.properties");
+     properties.load(input);
+     String BaseUrl = properties.getProperty("web.BaseURL");
+     
+    //navigate to URL
+	 driver.get(BaseUrl);
+	 test.info("Opened the website: https://www.google.com");
+
+	 Thread.sleep(1000);
+	 //download pdf file
+	 
+	 
+	//search for amazon india
+	 //driver.findElement(By.xpath("//textarea[@class='gLFyf']")).sendKeys("Amazon");
+	 Thread.sleep(1000);
+     System.out.println("test");
+     driver.close();
+     extent.flush();
   }
 }
